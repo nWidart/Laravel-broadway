937d3352a42cb2d874eaa859c30913bf84254a86
Nicolas Widart <n.widart@gmail.com>
2015-01-20 21:03:50 +0100

    Use the factory in the event store service provider

--- src/Nwidart/LaravelBroadway/Broadway/EventStorageServiceProvider.php
+++ src/Nwidart/LaravelBroadway/Broadway/EventStorageServiceProvider.php
@@ -1,40 +1,20 @@
 <?php namespace Nwidart\LaravelBroadway\Broadway;
 
-use Broadway\EventStore\DBALEventStore;
-use Doctrine\DBAL\Configuration;
-use Doctrine\DBAL\DriverManager;
 use Illuminate\Support\ServiceProvider;
 
 class EventStorageServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $connectionParams = $this->getStorageConnectionParameters();
-        $this->app->bind('Broadway\EventStore\EventStoreInterface', function ($app) use ($connectionParams) {
-            $configuration = new Configuration();
+        $this->app->bind(
+            'Nwidart\LaravelBroadway\EventStore\EventStoreFactory',
+            'Nwidart\LaravelBroadway\EventStore\Broadway\BroadwayEventStoreFactory'
+        );
 
-            $connection = DriverManager::getConnection($connectionParams, $configuration);
-            $payloadSerializer = $app['Broadway\Serializer\SerializerInterface'];
-            $metadataSerializer = $app['Broadway\Serializer\SerializerInterface'];
+        $this->app->bind('Broadway\EventStore\EventStoreInterface', function ($app) {
+            $driver = $app['config']->get('laravel-broadway::event-store.driver');
 
-            return new DBALEventStore($connection, $payloadSerializer, $metadataSerializer, 'event_store');
+            return $app['Nwidart\LaravelBroadway\EventStore\EventStoreFactory']->make($driver)->getDriver();
         });
-    }
-
-    /**
-     * Make a connection parameters array based on the laravel configuration
-     * @return array
-     */
-    private function getStorageConnectionParameters()
-    {
-        $driver = $this->app['config']->get('database.default');
-        $connectionParams = $this->app['config']->get("database.connections.{$driver}");
-
-        $connectionParams['dbname'] = $connectionParams['database'];
-        $connectionParams['user'] = $connectionParams['username'];
-        unset($connectionParams['database'], $connectionParams['username']);
-        $connectionParams['driver'] = "pdo_$driver";
-
-        return $connectionParams;
     }
 }
