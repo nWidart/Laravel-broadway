7205fee780245da7ef0dd7b79894a158f2282527
Nicolas Widart <n.widart@gmail.com>
2015-01-29 20:25:18 +0100

    Extract to methods

--- src/LaravelBroadwayServiceProvider.php
+++ src/LaravelBroadwayServiceProvider.php
@@ -6,9 +6,8 @@ class LaravelBroadwayServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $configPath = __DIR__ . '/../config/broadway.php';
-        $this->mergeConfigFrom($configPath, 'broadway');
-        $this->publishes([$configPath => config_path('broadway.php')]);
+        $this->registerConfiguration();
+        $this->registerMigrations();
 
         $this->app->register('Nwidart\LaravelBroadway\Broadway\EventServiceProvider');
         $this->app->register('Nwidart\LaravelBroadway\Broadway\CommandServiceProvider');
@@ -16,5 +15,26 @@ class LaravelBroadwayServiceProvider extends ServiceProvider
         $this->app->register('Nwidart\LaravelBroadway\Broadway\EventStorageServiceProvider');
         $this->app->register('Nwidart\LaravelBroadway\Broadway\SupportServiceProvider');
         $this->app->register('Nwidart\LaravelBroadway\Broadway\ReadModelServiceProvider');
+    }
+
+    /**
+     * Register the configuration file so Laravel can publish them
+     * Also merges the published config file with original
+     */
+    private function registerConfiguration()
+    {
+        $configPath = __DIR__ . '/../config/broadway.php';
+        $this->mergeConfigFrom($configPath, 'broadway');
+        $this->publishes([$configPath => config_path('broadway.php')]);
+    }
+
+    /**
+     * Register the migrations so Laravel can publish them
+     */
+    private function registerMigrations()
+    {
+        $this->publishes([
+            __DIR__.'/../migrations' => base_path('database/migrations')
+        ]);
     }
 }
